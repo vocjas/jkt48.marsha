eea52a84374bfcce600b079a41ed7785897bebf8
vocjas <162707724+vocjas@users.noreply.github.com>
2024-03-09 00:50:04 +0700

    Update owner.js

--- commands/owner.js
+++ commands/owner.js
@@ -34,7 +34,7 @@ module.exports = {
           iconURL: 'https://cdn.discordapp.com/attachments/1156866389819281418/1157310253520662638/2443-iconperson.png?ex=651824aa&is=6516d32a&hm=0becc4a0fda01e5a02a63cf098db30c287e60a474f8d2da4ddeae7f47d98a5a3&',
           url: 'https://discord.gg/abyrQwkWW5'
         })
-            .setDescription(`__**About me**__:\n\n ▶️ Myself Shiva aka RTX. I am a discord bot developer and web developer. I love playing games, watching anime and building different webserver applications. You will get faster replies on instagram than other social media. Find me on!\n Server : ❤️ [GUAVAAA](${youtubeLink})\n Instagram : 💙 [tegardanuartaaa](${InstagramLink})`)
+            .setDescription(`__**About me**__:\n\n ▶️ Nama acu danuu si gemaayy. Find me on!\n Home : ❤️ [GUAVAAA](${youtubeLink})\n Instagram : 💙 [tegardanuartaaa](${InstagramLink})`)
             .setTimestamp();
       interaction.reply({ embeds: [embed] }).catch(e => {});
 

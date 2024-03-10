41734e63097cf1b0a49f4cfedeb5ca9de985bfcc
vocjas <162707724+vocjas@users.noreply.github.com>
2024-03-10 15:59:33 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
   botInvite: "",
   supportServer: "",
   mongodbURL: "mongodb+srv://shiva:shiva@musicbotyt.ouljywv.mongodb.net/?retryWrites=true&w=majority",
-  status: 'AZIZI',
+  status: 'MARSHA',
   commandsDir: './commands',
   language: "en",
   embedColor: "00fbff",
